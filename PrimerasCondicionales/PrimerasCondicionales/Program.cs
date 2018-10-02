254b3c544a730a17eebdc59d173ce680bd608245
arrate7 <43437355+arrate7@users.noreply.github.com>
2018-10-02 18:49:48 +0200

    .

--- PrimerasCondicionales/PrimerasCondicionales/Program.cs
+++ PrimerasCondicionales/PrimerasCondicionales/Program.cs
@@ -14,7 +14,8 @@ namespace PrimerasCondicionales
              * Escribe un programa que pida dos números y que muestre la suma, 
              * la resta, la multiplicación, la media, el máximo y el mínimo.**/
 
-            int num1, num2, num3, segundos, minutos, horas, segundos2; 
+            int num1, num2, num3, segundos, minutos, horas, segundos2;
+            bool pagado, vip, saldoPositivo;
 
             //Console.WriteLine("Escribe un número");
             //num1 = Convert.ToInt32(Console.ReadLine());
@@ -67,37 +68,106 @@ namespace PrimerasCondicionales
              * Escribe un programa que convierta los segundos 
              * a horas, minutos y segundos.**/
 
-            Console.WriteLine("Introduce los segundos: ");
-            segundos=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Introduce los segundos: ");
+            //segundos=Convert.ToInt32(Console.ReadLine());
 
-            horas = segundos / 3600;
-            minutos = segundos % 3600 / 60;
-            segundos2 = segundos % 3600 % 60;
+            //horas = segundos / 3600;
+            //minutos = segundos % 3600 / 60;
+            //segundos2 = segundos % 3600 % 60;
 
 
-            Console.WriteLine(segundos + " segundos son " + horas + " h " + minutos + " m " + segundos2 + " s");
-            Console.ReadLine();
+            //Console.WriteLine(segundos + " segundos son " + horas + " h " + minutos + " m " + segundos2 + " s");
+            //Console.ReadLine();
 
             /**
-             * Escribe un programa que convierta los segundos 
-             * a horas, minutos y segundos.**/
+             * Escribe un programa que pida al usuario tres números y que diga si uno 
+             * de ellos es 20 menos que uno de los otros**/
 
+            //Console.WriteLine("Escribe un número");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Escribe otro número");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Escribe otro número");
+            //num3 = Convert.ToInt32(Console.ReadLine());
+
+            //if ((num1 + 20) == num2 )
+            //{
+            //    Console.WriteLine(num1 + " tiene 20 menos que " + num2);
+            //}
+
+            //if ((num1 + 20) == num3)
+            //{
+            //    Console.WriteLine(num1 + " tiene 20 menos que " + num3);
+            //}
+
+
+            //if ((num2 + 20) == num1)
+            //{
+            //    Console.WriteLine(num2 + " tiene 20 menos que " + num1);
+            //}
+            //if ((num2 + 20) == num3)
+            //{
+            //    Console.WriteLine(num2 + " tiene 20 menos que " + num3);
+            //}
+
+            //if ((num3 + 20) == num1)
+            //{
+            //    Console.WriteLine(num3 + " tiene 20 menos que " + num1);
+            //}
+            //if ((num3 + 20) == num2)
+            //{
+            //    Console.WriteLine(num3 + " tiene 20 menos que " + num2);
+            //}
+            //Console.ReadLine();
+
+            /**
+             * * Escribe un programa que pida dos números enteros entre 25 y 75 
+             * y que diga si hay un dígito en común entre los dos números.**/
+
+            //Console.WriteLine("Escribe un número");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Escribe otro número");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (num1 > 24 && num1 <76 && num2 > 24 && num2 < 76)
+            //{
+            //    if (num1 / 10 == num2 / 10)
+            //    {
+            //        Console.WriteLine("El número no está dentro del intervalo 25-75");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("El número no está dentro del intervalo 25-75");
+            //}
+
+            //Console.ReadLine();
+
+            /**
+               * * Vamos a escribir varios programas que saludan al usuario al iniciar. 
+               * El usuario tiene asociadas 3 variables, una que indica si ha pagado la entrada, 
+               * una que indica si es cliente VIP y la otro el saldo que tiene (positivo para saldo 
+               * de más, negativo para deuda). Haz un programa por cada punto:
+
+                    Comprobar si se ha pagado la entrada. Si se ha pagado, mostramos mensaje 
+                    "Bienvenido",si no mostrar mensaje "Tienes que pagar la entrada".
+
+                    A todos se les dice "Bienvenido", pero si es cliente VIP después de "Bienvenido" 
+                    también se le dice "Que pase un buen día" 
+
+                    Si no tiene deudas, le damos la bienvenida. En caso contrario no hacemos nada.
+
+                    Solo damos la bienvenida en el caso de cliente vip y el saldo sea mayor que 0
+                    **/
+                    
+
+          
+            Console.ReadLine();
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+    
 }
